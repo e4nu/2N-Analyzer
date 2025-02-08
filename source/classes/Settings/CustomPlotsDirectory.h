6a655d862e051f788392a83fd46ac0adeb9f479e
alons126 <alonsportes@mail.tau.ac.il>
2025-02-08 18:00:11 +0200

    C12 4.02962GeV - run 015778 - 1M events test

--- source/classes/Settings/CustomPlotsDirectory.h
+++ source/classes/Settings/CustomPlotsDirectory.h
@@ -29,7 +29,8 @@ class CustomPlotsDirectory {
 
     // string Global_Run_Status = "ChainRunV7_R3_100k_noRedef";
     // string Global_Run_Status = "ChainRunV7_R3_100k_original";
-    string Global_Run_Status = "ChainRunV7_R3_10M_original";
+    // string Global_Run_Status = "ChainRunV7_R3_10M_original";
+    string Global_Run_Status = "ChainRunV7_R3_full_original";
 
     // string Global_Run_Status = "ChainRunV7_R3_effTest_Rec_wTL_ES_1n_inEvent";
     // string Global_Run_Status = "ChainRunV7_R3_effTest_old";
