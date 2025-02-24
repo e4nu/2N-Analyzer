c73d49353653b390476aad7788f7c5c738caba46
alons126 <alonsportes@mail.tau.ac.il>
2025-02-24 09:53:37 +0200

    test

---
+++ nFD_eff_tester_run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo ""
+git pull
+echo ""
+echo "\033[35m- Re-pulling repository -----------------------------------------------\033[0m"
+echo ""
+git reset --hard
+git pull
+echo ""
+# echo "\033[35m- Cleaning excessive file ---------------------------------------------\033[0m"
+# echo ""
+# git clean -f
+# echo ""
+echo "\033[35m- Lunching nFD efficiency test code -----------------------------------\033[0m"
+echo ""
+clas12root -b -q testers/nFD_eff_test.cpp
+echo ""
+echo "\033[35m- Operation finished --------------------------------------------------\033[0m"
