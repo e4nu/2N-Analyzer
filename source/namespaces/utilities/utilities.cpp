5c8791eee735a217e2fc40749fa177b0d8204ccf
alons126 <alonsportes@mail.tau.ac.il>
2025-02-24 11:11:27 +0200

    test 666

---
+++ source/namespaces/utilities/utilities.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alon Sportes on 24/02/2025.
+//
+
+#include "utilities.h"
---
+++ source/namespaces/utilities/utilities.h
@@ -0,0 +1,8 @@
+//
+// Created by Alon Sportes on 24/02/2025.
+//
+
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#endif //UTILITIES_H
