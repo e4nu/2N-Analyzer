015406de20d4413d2bbfc2d6cf9f02cf6e273796
alons126 <alonsportes@mail.tau.ac.il>
2025-02-25 17:18:33 +0200

    nFD_eff_test_v3_WithPCALnVeto_r10000

--- testers/nFD_eff_test/nFD_eff_test.cpp
+++ testers/nFD_eff_test/nFD_eff_test.cpp
@@ -36,7 +36,7 @@ void nFD_eff_test() {
     // int Limiter = 10000; // 1 file
 
     // string OutFolderName = "nFD_eff_test_reg";
-    string OutFolderName = "nFD_eff_test_v3_WithPCALnVeto_r1500";
+    string OutFolderName = "nFD_eff_test_v3_WithPCALnVeto_r10000";
 
     const string OutputDir = "/lustre24/expphy/volatile/clas12/asportes/Analysis_output/" + OutFolderName;
     system(("rm -rf " + OutputDir).c_str());
