4d97b269cc76c13d9ce14faeb1e5c7a7a3f04f66
alons126 <alonsportes@mail.tau.ac.il>
2025-03-02 17:25:05 +0200

    _OnlyPeCuts

--- testers/nFD_eff_test/nFD_eff_test.cpp
+++ testers/nFD_eff_test/nFD_eff_test.cpp
@@ -56,10 +56,10 @@ void nFD_eff_test() {
 
             double P_upperLim;
 
-            // int Limiter = 25000000;  // 2500 files
+            int Limiter = 25000000;  // 2500 files
             // int Limiter = 10000000; // 1000 files
             // int Limiter = 1000000;  // 100 files
-            int Limiter = 100000;  // 10 files
+            // int Limiter = 100000;  // 10 files
             // int Limiter = 10000; // 1 file
 
             string OutFolderName_prefix = "nFD_eff_test";
