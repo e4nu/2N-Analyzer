58133ca47116235597b89c7006e29da07a7d6eb8
alons126 <alonsportes@mail.tau.ac.il>
2025-03-04 20:16:26 +0200

    _All_e_Cuts

--- testers/nFD_eff_test/nFD_eff_test.cpp
+++ testers/nFD_eff_test/nFD_eff_test.cpp
@@ -53,8 +53,8 @@ void nFD_eff_test() {
     // bool ConstrainedE = false;
     bool ConstrainedE = true;
 
-    bool OnlyGood_nFD = true;
-    bool OnlyBad_nFD = false;
+    bool OnlyGood_nFD = false;
+    bool OnlyBad_nFD = true;
 
     // bool plot_AMaps = false;
     bool plot_AMaps = true;
