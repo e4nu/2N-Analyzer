bd76111c37e46f0a7f5c8a6a4bc5dad1b3be9130
alons126 <alonsportes@mail.tau.ac.il>
2025-03-05 15:33:07 +0200

    start_screen_session test 11

--- setup/scripts/code_updater.sh
+++ setup/scripts/code_updater.sh
@@ -7,12 +7,14 @@ unset COLOR_END
 setenv COLOR_END '\033[0m'
 
 echo ""
-git pull
-echo ""
 echo "${COLOR_START}- Re-pulling repository -----------------------------------------------${COLOR_END}"
+echo "${COLOR_START}- Pulling repository --------------------------------------------------${COLOR_END}"
 echo ""
 git reset --hard
 git pull
+echo ""
+echo "${COLOR_START}HEAD:${COLOR_END}"
+git log -1 --oneline
 echo ""
 echo "${COLOR_START}- Cleaning excessive file ---------------------------------------------${COLOR_END}"
 echo ""
