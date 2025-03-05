4f229885dc539b48d166298b460cc9311d174073
alons126 <alonsportes@mail.tau.ac.il>
2025-03-05 15:22:42 +0200

    start_screen_session test 10

--- setup/scripts/code_updater.sh
+++ setup/scripts/code_updater.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-source ./setup/scripts/environment/set_environment.csh
+unset COLOR_START
+setenv COLOR_START '\033[35m'
+
+unset COLOR_END
+setenv COLOR_END '\033[0m'
 
 echo ""
 git pull
@@ -10,9 +14,10 @@ echo ""
 git reset --hard
 git pull
 echo ""
-# echo "${COLOR_START}- Cleaning excessive file ---------------------------------------------${COLOR_END}"
-# echo ""
-# git clean -f
-# echo ""
+echo "${COLOR_START}- Cleaning excessive file ---------------------------------------------${COLOR_END}"
+echo ""
+git clean -f
+echo ""
 
+source ./setup/scripts/environment/set_environment.csh
 source ./setup/scripts/screen/setup_screen_commands.csh
