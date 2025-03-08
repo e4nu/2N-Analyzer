7f78be47b5a4c529c8c4394c8ea68bec0a298630
alons126 <alonsportes@mail.tau.ac.il>
2025-03-08 15:15:07 +0200

    08_new_FTOF_veto_test_WITH_Neutral_veto

--- testers/nFD_eff_test/nFD_eff_test.cpp
+++ testers/nFD_eff_test/nFD_eff_test.cpp
@@ -44,7 +44,7 @@ void nFD_eff_test() {
 
     bool apply_neutFD_redef = true;
     bool apply_ECAL_veto = true;
-    bool apply_PCAL_neutral_veto = false;
+    bool apply_PCAL_neutral_veto = true;
 
     bool No_FTOF_hits = false;
 
@@ -57,7 +57,7 @@ void nFD_eff_test() {
     // bool plot_AMaps = false;
     bool plot_AMaps = true;
 
-    string OutFolderName_prefix = "07_new_FTOF_veto_test_NO_VETO";
+    string OutFolderName_prefix = "08_new_FTOF_veto_test_WITH_Neutral_veto";
     // string OutFolderName_prefix = "06_No_FTOF_cuts_test";
     // string OutFolderName_prefix = "05_test";
     string OutFolderName_ver_status = "_v18";
