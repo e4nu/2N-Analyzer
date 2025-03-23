a4e1938363b7246aa764a8774e5abbea9d921fe1
alons126 <alonsportes@mail.tau.ac.il>
2025-03-23 17:29:47 -0400

    test run 28

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ source update_only.sh
 
 echo "\033[35m- Lunching data-analysis code -----------------------------------------\033[0m"
 echo ""
-clas12root -b -q './main.c(" -DIFARM_RUN")'
+clas12root -b -q './main.c+(" -DIFARM_RUN")'
 # clas12root -b -q ./main.c
 echo ""
 echo "\033[35m- Operation finished --------------------------------------------------\033[0m"
