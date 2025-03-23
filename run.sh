29677a12aab09ad9f15cd03eeab0981f820031c1
alons126 <alonsportes@mail.tau.ac.il>
2025-03-23 16:16:07 -0400

    test run 22

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ source update_only.sh
 
 echo "\033[35m- Lunching data-analysis code -----------------------------------------\033[0m"
 echo ""
-clas12root -b -q ./main.c
+clas12root -b -q ./main.cpp
+# clas12root -b -q ./main.c
 echo ""
 echo "\033[35m- Operation finished --------------------------------------------------\033[0m"
 echo ""
