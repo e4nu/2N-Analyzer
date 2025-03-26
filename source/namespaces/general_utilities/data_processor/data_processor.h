627eed8bd008d316a04913a3b70594aaa57670ac
alons126 <alonsportes@mail.tau.ac.il>
2025-03-26 13:05:58 +0200

    test run 55

--- source/namespaces/general_utilities/data_processor/data_processor.h
+++ source/namespaces/general_utilities/data_processor/data_processor.h
@@ -483,6 +483,59 @@ std::string SetSaveDir(const std::string &Source, const std::string &BaseSaveDir
     }
 }
 
+void SetSaveDir(std::string &SaveDir, std::string &TestSaveDir, const std::string &RecTitle, const std::string &Histogram1DSaveNamePath, const std::string &PlotsT,
+                const std::string &DRegion, const std::string &Particle, const std::string &ParticleLC, const std::string &ParticleShort, const std::string &Type) {
+    if (PlotsT == "FSRatio") {
+        if (!findSubstring(Type, "vs") && !findSubstring(Type, "vs.") && !findSubstring(Type, "VS") && !findSubstring(Type, "VS.")) {
+            if (Type == "W" || Type == "Q2" || Type == "E_e" || Type == "omega" || Type == "Ecal" || Type == "deltaP_T_tot" || Type == "deltaP_T_L" || Type == "deltaAlpha_T_tot" ||
+                Type == "deltaAlpha_T_L" || Type == "deltaPhi_T_tot" || Type == "deltaPhi_T_L" || Type == "total_3momentum" || Type == "relative_3momentum" || Type == "total_4momentum" ||
+                Type == "relative_4momentum" || Type == "leading_nuc_momentum" || Type == "recoil_nuc_momentum" || Type == "theta_tot" || Type == "phi_tot" || Type == "theta_rel" ||
+                Type == "phi_rel" || Type == "Opening_ang_P_nucL_minus_q_nucR" || Type == "P_tot_minus_q") {
+                SaveDir = Histogram1DSaveNamePath + "/";
+                TestSaveDir = SaveDir + "Cloned_hist_test/";
+            } else {
+                if (findSubstring(RecTitle, "Electron") || findSubstring(RecTitle, "electron")) {
+                    SaveDir = Histogram1DSaveNamePath + "/00_" + Particle + "_" + Type + "_plots/";
+                    TestSaveDir = SaveDir + "Cloned_hist_test/";
+                } else {
+                    if (DRegion == "FD") {
+                        SaveDir = Histogram1DSaveNamePath + "/01_FD_Nucleon_" + Type + "_plots/";
+                        TestSaveDir = SaveDir + "Cloned_hist_test/";
+                    } else if (DRegion == "CD") {
+                        SaveDir = Histogram1DSaveNamePath + "/02_CD_Nucleon_" + Type + "_plots/";
+                        TestSaveDir = SaveDir + "Cloned_hist_test/";
+                    } else {
+                        SaveDir = Histogram1DSaveNamePath + "/" + Particle + "_" + Type + "_plots/";
+                        TestSaveDir = SaveDir + "Cloned_hist_test" + "/";
+                    }
+                }
+            }
+        } else {
+            SaveDir = Histogram1DSaveNamePath + "/" + Type + "_plots/";
+            TestSaveDir = SaveDir + "Cloned_hist_test/";
+        }
+    } else {
+        if (findSubstring(RecTitle, "Electron") || findSubstring(RecTitle, "electron")) {
+            SaveDir = Histogram1DSaveNamePath + "/00_" + Particle + "_" + Type + "_plots/";
+            TestSaveDir = SaveDir + "Cloned_hist_test/";
+        } else {
+            if (findSubstring(RecTitle, ", FD)") || findSubstring(RecTitle, "FD " + Particle) || findSubstring(RecTitle, "FD " + ParticleLC)) {
+                SaveDir = Histogram1DSaveNamePath + "/01_FD_" + Particle + "_" + Type + "_plots/";
+                TestSaveDir = SaveDir + "Cloned_hist_test/";
+            } else if (findSubstring(RecTitle, ", CD)") || findSubstring(RecTitle, "CD " + Particle) || findSubstring(RecTitle, "CD " + ParticleLC)) {
+                SaveDir = Histogram1DSaveNamePath + "/02_CD_" + Particle + "_" + Type + "_plots/";
+                TestSaveDir = SaveDir + "Cloned_hist_test/";
+            } else {
+                SaveDir = Histogram1DSaveNamePath + "/" + Particle + "_" + Type + "_plots/";
+                TestSaveDir = SaveDir + "Cloned_hist_test" + "/";
+            }
+        }
+    }
+
+    system(("mkdir -p " + SaveDir).c_str());
+    system(("mkdir -p " + TestSaveDir).c_str());
+}
+
 // SetFSRatioSaveDir function -------------------------------------------------------------------------------------------------------------------------------------------
 
 void SetFSRatioSaveDir(std::string &SaveDir, std::string &TestSaveDir, const std::string &RecTitle, const std::string &FinalState, const std::string &Histogram1DSaveNamePathOriginal,
