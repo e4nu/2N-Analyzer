739e98e81325d08af1c33cd7e57a58a92aee424f
alons126 <alonsportes@mail.tau.ac.il>
2025-03-27 14:41:54 +0200

    new setup namespace test 12

--- framework/includes/work_dir_include.h
+++ framework/includes/work_dir_include.h
@@ -5,21 +5,19 @@
 // #ifndef WORK_DIR_INCLUDE_H
 // #define WORK_DIR_INCLUDE_H
 
-#include <iostream>
-
 #ifdef WORK_DIR
-std::cout << "WORK_DIR is defined: " << WORK_DIR << std::endl;
 #define RECO_ANALYSIS_RUN true
 #else
-std::cout << "WORK_DIR is not defined, using default: " << workDir << std::endl;
 #define RECO_ANALYSIS_RUN false
 #endif
 
+#include <iostream>
+
 void RECO_ANALYSIS_RUN_PrintOut() {
 #if RECO_ANALYSIS_RUN
-    std::cout << "\033[33m\nRunning in ifarm configuration (WORK_DIR = " << WORK_DIR << "; RECO_ANALYSIS_RUN = " << basic_tools::BoolToString(RECO_ANALYSIS_RUN) << ")\n";
+    std::cout << "\033[33m\nRunning in ifarm configuration (WORK_DIR = " << WORK_DIR << "; RECO_ANALYSIS_RUN = " << RECO_ANALYSIS_RUN << ")\n";
 #else
-    std::cout << "\033[33m\nRunning in gpvm configuration (WORK_DIR = " << WORK_DIR << "; RECO_ANALYSIS_RUN = " << basic_tools::BoolToString(RECO_ANALYSIS_RUN) << ")\n\n";
+    std::cout << "\033[33m\nRunning in gpvm configuration (WORK_DIR = " << WORK_DIR << "; RECO_ANALYSIS_RUN = " << RECO_ANALYSIS_RUN << ")\n\n";
 #endif
 }
 
