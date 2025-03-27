15b49ebee00c270f31e80ed23c77b3a75c1cb08f
alons126 <alonsportes@mail.tau.ac.il>
2025-03-27 13:03:06 +0200

    new setup namespace test 1

---
+++ framework/namespaces/settings/debugging_settings/debugging_settings.h
@@ -0,0 +1,8 @@
+//
+// Created by Alon Sportes on 27/03/2025.
+//
+
+#ifndef DEBUGGING_SETTINGS_H
+#define DEBUGGING_SETTINGS_H
+
+#endif //DEBUGGING_SETTINGS_H
---
+++ framework/namespaces/setup/debugging/debugging.h
@@ -0,0 +1,8 @@
+//
+// Created by Alon Sportes on 27/03/2025.
+//
+
+#ifndef DEBUGGING_H
+#define DEBUGGING_H
+
+#endif //DEBUGGING_H
