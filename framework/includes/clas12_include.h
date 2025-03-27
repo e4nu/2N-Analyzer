af9c1f1645d59002b5fca4e234adf75023a1f53e
alons126 <alonsportes@mail.tau.ac.il>
2025-03-27 16:36:34 +0200

    new setup namespace test 32

---
+++ framework/includes/clas12_include.h
@@ -0,0 +1,9 @@
+#ifndef CLAS12_INCLUDE_H
+#define CLAS12_INCLUDE_H
+
+#include "clas12reader.h"
+#include "region_particle.h"
+
+using namespace clas12;
+
+#endif // CLAS12_INCLUDE_H
--- framework/namespaces/general_utilities/utilities/utilities_include_rm.h
+++
@@ -1,5 +0,0 @@
-#ifdef RECO_ANALYSIS_RUN
-#include "utilities_reco_extentions.h"
-#else
-#include "utilities.h"
-#endif
