f957f154812648397084577cf8338d033b1fc411
alons126 <alonsportes@mail.tau.ac.il>
2025-03-27 14:28:21 +0200

    new setup namespace test 8

--- framework/structures/CodeDirectories/CodeDirectories.h
+++ framework/structures/CodeDirectories/CodeDirectories.h
@@ -10,30 +10,47 @@
 #include "../../namespaces/general_utilities/basic_tools/basic_tools.h"
 
 struct CodeDirectories {
-    const std::string WorkingDirectory;
-    const std::string plots_path;
-    const std::string SettingsDirectory;
-    const std::string SourceDirectory;
-    const std::string DataDirectory;
-    const std::string ACorrDirectory;
-    const std::string AcceptanceMapsDirectory;
-    const std::string AcceptanceWeightsDirectory;
-    const std::string MomentumResolutionDirectory;
-    const std::string NucleonCutsDirectory;
-    const std::string PIDCutsDirectory;
+    std::string WorkingDirectory;
+    std::string plots_path;
+    std::string SettingsDirectory;
+    std::string SourceDirectory;
+    std::string DataDirectory;
+    std::string ACorrDirectory;
+    std::string AcceptanceMapsDirectory;
+    std::string AcceptanceWeightsDirectory;
+    std::string MomentumResolutionDirectory;
+    std::string NucleonCutsDirectory;
+    std::string PIDCutsDirectory;
 
-    CodeDirectories()
-        : WorkingDirectory(basic_tools::GetCurrentDirectory() + "/"),
-          plots_path("/lustre24/expphy/volatile/clas12/asportes/Analysis_output"),
-          SettingsDirectory(WorkingDirectory + "setup/"),
-          SourceDirectory(WorkingDirectory + "source/"),
-          DataDirectory(SourceDirectory + "data/"),
-          ACorrDirectory(DataDirectory + "AcceptanceCorrections/"),
-          AcceptanceMapsDirectory(DataDirectory + "AcceptanceMaps/"),
-          AcceptanceWeightsDirectory(DataDirectory + "AcceptanceWeights/"),
-          MomentumResolutionDirectory(DataDirectory + "NeutronResolution/"),
-          NucleonCutsDirectory(DataDirectory + "NucleonCuts/"),
-          PIDCutsDirectory(DataDirectory + "PIDCuts/") {}
+    CodeDirectories() {
+        WorkingDirectory(basic_tools::GetCurrentDirectory() + "/");
+        plots_path("/lustre24/expphy/volatile/clas12/asportes/Analysis_output");
+
+        SettingsDirectory(WorkingDirectory + "setup/");
+
+        SourceDirectory(WorkingDirectory + "source/");
+
+        DataDirectory(SourceDirectory + "data/");
+        ACorrDirectory(DataDirectory + "AcceptanceCorrections/");
+
+        AcceptanceMapsDirectory(DataDirectory + "AcceptanceMaps/");
+        AcceptanceWeightsDirectory(DataDirectory + "AcceptanceWeights/");
+        MomentumResolutionDirectory(DataDirectory + "NeutronResolution/");
+        NucleonCutsDirectory(DataDirectory + "NucleonCuts/");
+        PIDCutsDirectory(DataDirectory + "PIDCuts/");
+    }
+    // CodeDirectories()
+    //     : WorkingDirectory(basic_tools::GetCurrentDirectory() + "/"),
+    //       plots_path("/lustre24/expphy/volatile/clas12/asportes/Analysis_output"),
+    //       SettingsDirectory(WorkingDirectory + "setup/"),
+    //       SourceDirectory(WorkingDirectory + "source/"),
+    //       DataDirectory(SourceDirectory + "data/"),
+    //       ACorrDirectory(DataDirectory + "AcceptanceCorrections/"),
+    //       AcceptanceMapsDirectory(DataDirectory + "AcceptanceMaps/"),
+    //       AcceptanceWeightsDirectory(DataDirectory + "AcceptanceWeights/"),
+    //       MomentumResolutionDirectory(DataDirectory + "NeutronResolution/"),
+    //       NucleonCutsDirectory(DataDirectory + "NucleonCuts/"),
+    //       PIDCutsDirectory(DataDirectory + "PIDCuts/") {}
 };  // struct CodeDirectories
 
 #endif  // CODEDIRECTORIES_H
