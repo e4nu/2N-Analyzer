515fbe94a71c2d08e8c880e1319313b1df31d4a0
alons126 <alonsportes@mail.tau.ac.il>
2025-03-27 14:18:20 +0200

    new setup namespace test 6

---
+++ framework/structures/CodeDirectories/CodeDirectories.h
@@ -0,0 +1,39 @@
+//
+// Created by Alon Sportes on 27/03/2025.
+//
+
+#ifndef CODEDIRECTORIES_H
+#define CODEDIRECTORIES_H
+
+#include <string>
+
+#include "../../namespaces/general_utilities/basic_tools/basic_tools.h"
+
+struct PathDefinitions {
+    const std::string WorkingDirectory;
+    const std::string plots_path;
+    const std::string SettingsDirectory;
+    const std::string SourcesDirectory;
+    const std::string DataDirectory;
+    const std::string ACorrDirectory;
+    const std::string AcceptanceMapsDirectory;
+    const std::string AcceptanceWeightsDirectory;
+    const std::string MomentumResolutionDirectory;
+    const std::string NucleonCutsDirectory;
+    const std::string PIDCutsDirectory;
+
+    PathDefinitions()
+        : WorkingDirectory(basic_tools::GetCurrentDirectory() + "/"),
+          plots_path("/lustre24/expphy/volatile/clas12/asportes/Analysis_output"),
+          SettingsDirectory(WorkingDirectory + "setup/"),
+          SourcesDirectory(WorkingDirectory + "source/"),
+          DataDirectory(SourcesDirectory + "data/"),
+          ACorrDirectory(DataDirectory + "AcceptanceCorrections/"),
+          AcceptanceMapsDirectory(DataDirectory + "AcceptanceMaps/"),
+          AcceptanceWeightsDirectory(DataDirectory + "AcceptanceWeights/"),
+          MomentumResolutionDirectory(DataDirectory + "NeutronResolution/"),
+          NucleonCutsDirectory(DataDirectory + "NucleonCuts/"),
+          PIDCutsDirectory(DataDirectory + "PIDCuts/") {}
+};
+
+#endif //CODEDIRECTORIES_H
