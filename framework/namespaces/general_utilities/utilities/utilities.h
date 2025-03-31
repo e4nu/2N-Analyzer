2f277dd531add8c8ecb0d68f29028e404c942654
alons126 <alonsportes@mail.tau.ac.il>
2025-03-31 14:48:48 +0300

    back from cmake 13

--- framework/namespaces/general_utilities/utilities/utilities.h
+++ framework/namespaces/general_utilities/utilities/utilities.h
@@ -27,7 +27,7 @@
 #include <string>
 #include <tuple>
 
-#include "../analysis_math/analysis_math_include.cpp"
+#include "../analysis_math/analysis_math_include.h"
 #include "../basic_tools/basic_tools_include.h"
 #include "../constants/constants_include.h"
 #include "../data_processor/data_processor.cpp"
