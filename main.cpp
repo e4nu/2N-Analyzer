788bf601f2210e4504a7dea3d701aba3c2118be5
alons126 <alonsportes@mail.tau.ac.il>
2025-04-01 08:22:22 -0400

    amaps tests 0

--- main.cpp
+++ main.cpp
@@ -1,12 +1,64 @@
-#include "TChain.h"
-#include "TSelector.h"
-#include "TROOT.h"
-#include "TSystem.h"
-#include "TApplication.h"
 #include <iostream>
 
-int main() {
-    TChain RootChain("chain");
+#include "framework/namespaces/setup/code_setup.h"
 
-    RootChain.Process("RunCode.cpp+");
+#include "framework/classes/DataAnalyser/DataAnalyser.cpp"
+#include "framework/namespaces/general_utilities/basic_tools.h"
+
+// using namespace std;
+using namespace code_setup;
+
+void RunCode() {
+    code_setup::ConfigSampleChain();
+    ConfigCanvasPDF();
+
+    auto start = std::chrono::system_clock::now();  // Start counting running time
+
+    int Num_of_analysed_samples = 0;
+    
+    cout << "\nLooping over sample chain...\n";
+    
+    for (int i = 0; i < AnalyseFilePath_v.size(); i++) {
+        std::string AnalyseFilePath0 = AnalyseFilePath_v.at(i);
+        std::string AnalyseFileSample0 = AnalyseFileSample_v.at(i);
+        std::string AnalyseFileDir0 = AnalyseFileDir_v.at(i);
+        std::string AnalyseFile0 = AnalyseFile_v.at(i);
+    
+        std::string FileType = AnalyseFile_v.at(i).substr(AnalyseFile_v.at(i).find_last_of(".") + 1);
+    
+        cout << "\nAnalyseFilePath:\t" << AnalyseFilePath0 << "\n";
+        cout << "AnalyseFileSample:\t" << AnalyseFileSample0 << "\n";
+        cout << "AnalyseFileDir:\t" << AnalyseFileDir0 << "\n";
+        cout << "AnalyseFile:\t" << AnalyseFile0 << "\n\n";
+    
+        cout << "FileType:\t" << FileType << "\n";
+    
+        DataAnalyser Analysis(FileType, AnalyseFilePath0, AnalyseFileSample0, AnalyseFile0);
+        std::string AnalyserMode = Analysis.ConfigureAnalyserMode(FileType);
+    
+        cout << "Analyser mode:\t'" << AnalyserMode << "'\n";
+    
+        cout << "\n---------------------------------------------------------------------------\n\n";
+        cout << "Analysis mode:\t'" << AnalyserMode << "'" << "\n";
+        cout << "Code version:\t" << Ver << "\n";
+    
+        ++Num_of_analysed_samples;
+    
+        if (AnalyseFilePath_v.size() > 1) {  // Delete all ROOT objects whose class names start with TH (to prevent a memory leak)
+            // gDirectory->Delete("TH*;*");
+            gDirectory->Clear();
+        }
+    }
+
+    cout << "#(analysed samples):\t" << Num_of_analysed_samples << "\n";
+
+    auto end = std::chrono::system_clock::now();
+    auto elapsed_time_seconds = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+    double elapsed_time_minutes = elapsed_time_seconds.count() / 60;
+
+    if (elapsed_time_seconds.count() < 60) {
+        std::cout << "Running time:\t" << elapsed_time_seconds.count() << " seconds\n\n";
+    } else {
+        std::cout << "Running time:\t" << basic_tools::ToStringWithPrecision(elapsed_time_minutes, 3) << " minutes\n\n";
+    }
 }
