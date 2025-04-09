1165c26f2ae7aa9b8d6bc4cd974bb655291e92cf
alons126 <alonsportes@mail.tau.ac.il>
2025-04-10 00:18:25 +0300

    enabled IsInFDQuery

--- framework/classes/AMaps/AMaps.cpp
+++ framework/classes/AMaps/AMaps.cpp
@@ -3245,12 +3245,11 @@ bool AMaps::IsInFDQuery(bool Generate_AMaps, const DSCuts &ThetaFD, const std::s
 
     bool inFDQuery, part_inSomeSector;
 
-    part_inSomeSector = true;
-    // if (!Generate_AMaps) {
-    //     part_inSomeSector = MatchAngToHitMap(Particle, Momentum, Theta, Phi, NucleonOverlappingFC);
-    // } else {
-    //     part_inSomeSector = true;
-    // }
+    if (!Generate_AMaps) {
+        part_inSomeSector = MatchAngToHitMap(Particle, Momentum, Theta, Phi, NucleonOverlappingFC);
+    } else {
+        part_inSomeSector = true;
+    }
 
     inFDQuery = (part_inSomeSector && (Theta >= ThetaFD.GetLowerCutConst()) && (Theta <= ThetaFD.GetUpperCutConst()));
 
