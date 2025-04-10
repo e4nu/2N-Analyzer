700cf3e78d4024457ecf17d4196c698af4e667f3
alons126 <alonsportes@mail.tau.ac.il>
2025-04-10 16:10:52 +0300

    Added a function to save histograms to PDF (untested!)

--- applications/RecoAnalyzer.cpp
+++ applications/RecoAnalyzer.cpp
@@ -25385,72 +25385,74 @@ RecoAnalyzer::RecoAnalyzer(const std::string &AnalyzeFilePath, const std::string
 
 #pragma endregion
 
-    // Saving histogram PDF file ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Saving histograms to PDF file ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    // Create the output PDFs
-    int pixelx = 1980;
-    int pixely = 1530;
-    TCanvas *TextCanvas = new TCanvas("TextCanvas", "TextCanvas", pixelx, pixely);
-    TLatex titles;
-    TLatex text;
-    titles.SetTextSize(0.1);
-    text.SetTextSize(0.05);
+    histogram_functions::DrawAndSaveHistogramsToPDF(MainCanvas, HistoList, Histogram_OutPDF_fileName_str, Histogram_OutPDF_fileName_char, SampleName, VaryingSampleName, beamE);
 
-    sprintf(Histogram_OutPDF_fileName_char, "%s[", Histogram_OutPDF_fileName_str.c_str());
-    TextCanvas->SaveAs(Histogram_OutPDF_fileName_char);
-    sprintf(Histogram_OutPDF_fileName_char, "%s", Histogram_OutPDF_fileName_str.c_str());
+    // // Create the output PDFs
+    // int pixelx = 1980;
+    // int pixely = 1530;
+    // TCanvas *TextCanvas = new TCanvas("TextCanvas", "TextCanvas", pixelx, pixely);
+    // TLatex titles;
+    // TLatex text;
+    // titles.SetTextSize(0.1);
+    // text.SetTextSize(0.05);
 
-    TextCanvas->cd();
+    // sprintf(Histogram_OutPDF_fileName_char, "%s[", Histogram_OutPDF_fileName_str.c_str());
+    // TextCanvas->SaveAs(Histogram_OutPDF_fileName_char);
+    // sprintf(Histogram_OutPDF_fileName_char, "%s", Histogram_OutPDF_fileName_str.c_str());
 
-    titles.DrawLatex(0.05, 0.90, "2N analyzer output");
-    text.DrawLatex(0.05, 0.80, parameters.SampleName.c_str());
-    text.DrawLatex(0.05, 0.75, parameters.VaryingSampleName.c_str());
-    text.DrawLatex(0.2, 0.65, ("Beam energy: " + basic_tools::ToStringWithPrecision(parameters.beamE, 3) + " [GeV]").c_str());
+    // TextCanvas->cd();
 
-    TextCanvas->Print(Histogram_OutPDF_fileName_char, "pdf");
-    TextCanvas->Clear();
+    // titles.DrawLatex(0.05, 0.90, "2N analyzer output");
+    // text.DrawLatex(0.05, 0.80, parameters.SampleName.c_str());
+    // text.DrawLatex(0.05, 0.75, parameters.VaryingSampleName.c_str());
+    // text.DrawLatex(0.2, 0.65, ("Beam energy: " + basic_tools::ToStringWithPrecision(parameters.beamE, 3) + " [GeV]").c_str());
 
-    MainCanvas->cd();
-    MainCanvas->Clear();
+    // TextCanvas->Print(Histogram_OutPDF_fileName_char, "pdf");
+    // TextCanvas->Clear();
 
-    for (int i = 0; i < HistoList.size(); i++) {
-        MainCanvas->cd();
+    // MainCanvas->cd();
+    // MainCanvas->Clear();
 
-        MainCanvas->cd()->SetGrid();
-        MainCanvas->cd()->SetBottomMargin(0.14);
-        MainCanvas->cd()->SetLeftMargin(0.16);
-        MainCanvas->cd()->SetRightMargin(0.12);
+    // for (int i = 0; i < HistoList.size(); i++) {
+    //     MainCanvas->cd();
 
-        if (HistoList[i]->InheritsFrom("TH1")) {
-            TH1 *h1 = dynamic_cast<TH1 *>(HistoList[i]);
-            h1->GetYaxis()->SetTitleOffset(1.5);
-            h1->GetXaxis()->SetTitleOffset(1.1);
-            gPad->SetRightMargin(0.23);
+    //     MainCanvas->cd()->SetGrid();
+    //     MainCanvas->cd()->SetBottomMargin(0.14);
+    //     MainCanvas->cd()->SetLeftMargin(0.16);
+    //     MainCanvas->cd()->SetRightMargin(0.12);
 
-            if (HistoList[i]->InheritsFrom("TH1D") || HistoList[i]->InheritsFrom("THStack")) { h1->Draw(); }
+    //     if (HistoList[i]->InheritsFrom("TH1")) {
+    //         TH1 *h1 = dynamic_cast<TH1 *>(HistoList[i]);
+    //         h1->GetYaxis()->SetTitleOffset(1.5);
+    //         h1->GetXaxis()->SetTitleOffset(1.1);
+    //         gPad->SetRightMargin(0.23);
 
-        } else if (HistoList[i]->InheritsFrom("TH2")) {
-            TH2 *h2 = dynamic_cast<TH2 *>(HistoList[i]);
-            h2->GetYaxis()->SetTitleOffset(1.5);
-            h2->GetXaxis()->SetTitleOffset(1.1);
-            gPad->SetRightMargin(0.23);
-            h2->Draw("colz");
+    //         if (HistoList[i]->InheritsFrom("TH1D") || HistoList[i]->InheritsFrom("THStack")) { h1->Draw(); }
 
-            if (h2->GetEntries() != 0) {
-                gPad->Update();
-                TPaletteAxis *palette = (TPaletteAxis *)h2->GetListOfFunctions()->FindObject("palette");
-                if (palette) palette->SetY2NDC(0.65);
-                gPad->Modified();
-                gPad->Update();
-            }
-        }
+    //     } else if (HistoList[i]->InheritsFrom("TH2")) {
+    //         TH2 *h2 = dynamic_cast<TH2 *>(HistoList[i]);
+    //         h2->GetYaxis()->SetTitleOffset(1.5);
+    //         h2->GetXaxis()->SetTitleOffset(1.1);
+    //         gPad->SetRightMargin(0.23);
+    //         h2->Draw("colz");
 
-        MainCanvas->Print(Histogram_OutPDF_fileName_char, "pdf");
-        MainCanvas->Clear();
-    }
+    //         if (h2->GetEntries() != 0) {
+    //             gPad->Update();
+    //             TPaletteAxis *palette = (TPaletteAxis *)h2->GetListOfFunctions()->FindObject("palette");
+    //             if (palette) palette->SetY2NDC(0.65);
+    //             gPad->Modified();
+    //             gPad->Update();
+    //         }
+    //     }
 
-    sprintf(Histogram_OutPDF_fileName_char, "%s]", Histogram_OutPDF_fileName_str.c_str());
-    MainCanvas->Print(Histogram_OutPDF_fileName_char, "pdf");
+    //     MainCanvas->Print(Histogram_OutPDF_fileName_char, "pdf");
+    //     MainCanvas->Clear();
+    // }
+
+    // sprintf(Histogram_OutPDF_fileName_char, "%s]", Histogram_OutPDF_fileName_str.c_str());
+    // MainCanvas->Print(Histogram_OutPDF_fileName_char, "pdf");
 
     // Saving histogram list and finishing execution ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
--- framework/namespaces/general_utilities/histogram_functions.h
+++ framework/namespaces/general_utilities/histogram_functions.h
@@ -63,6 +63,79 @@ void FillByInt2D(TH2D *H1D_All_Int, TH2D *H1D_QEL, TH2D *H1D_MEC, TH2D *H1D_RES,
     }
 }
 
+// GetHistogramFromVector function --------------------------------------------------------------------------------------------------------------------------------------
+
+// Function to render and save histograms from a list to a PDF
+void DrawAndSaveHistogramsToPDF(TCanvas *MainCanvas, const std::vector<TObject *> &HistoList, const std::string &Histogram_OutPDF_fileName_str, char *Histogram_OutPDF_fileName_char,
+                                const std::string &SampleName, const std::string &VaryingSampleName, const double &beamE) {
+    int pixelx = 1980;
+    int pixely = 1530;
+    TCanvas *TextCanvas = new TCanvas("TextCanvas", "TextCanvas", pixelx, pixely);
+    TLatex titles;
+    TLatex text;
+    titles.SetTextSize(0.1);
+    text.SetTextSize(0.05);
+
+    sprintf(Histogram_OutPDF_fileName_char, "%s[", Histogram_OutPDF_fileName_str.c_str());
+    TextCanvas->SaveAs(Histogram_OutPDF_fileName_char);
+    sprintf(Histogram_OutPDF_fileName_char, "%s", Histogram_OutPDF_fileName_str.c_str());
+
+    TextCanvas->cd();
+
+    titles.DrawLatex(0.05, 0.90, "2N analyzer output");
+    text.DrawLatex(0.05, 0.80, SampleName.c_str());
+    text.DrawLatex(0.05, 0.75, VaryingSampleName.c_str());
+    text.DrawLatex(0.2, 0.65, ("Beam energy: " + basic_tools::GetBeamEnergyFromDouble(beamE) + " [GeV]").c_str());
+
+    TextCanvas->Print(Histogram_OutPDF_fileName_char, "pdf");
+    TextCanvas->Clear();
+
+    MainCanvas->cd();
+    MainCanvas->Clear();
+
+    for (int i = 0; i < HistoList.size(); i++) {
+        MainCanvas->cd();
+
+        MainCanvas->cd()->SetGrid();
+        MainCanvas->cd()->SetBottomMargin(0.14);
+        MainCanvas->cd()->SetLeftMargin(0.16);
+        MainCanvas->cd()->SetRightMargin(0.12);
+
+        if (HistoList[i]->InheritsFrom("TH1")) {
+            TH1 *h1 = dynamic_cast<TH1 *>(HistoList[i]);
+            if (!h1) continue;
+            h1->GetYaxis()->SetTitleOffset(1.5);
+            h1->GetXaxis()->SetTitleOffset(1.1);
+            gPad->SetRightMargin(0.23);
+
+            h1->Draw();
+
+        } else if (HistoList[i]->InheritsFrom("TH2")) {
+            TH2 *h2 = dynamic_cast<TH2 *>(HistoList[i]);
+            if (!h2) continue;
+            h2->GetYaxis()->SetTitleOffset(1.5);
+            h2->GetXaxis()->SetTitleOffset(1.1);
+            gPad->SetRightMargin(0.23);
+
+            h2->Draw("colz");
+
+            if (h2->GetEntries() != 0) {
+                gPad->Update();
+                TPaletteAxis *palette = (TPaletteAxis *)h2->GetListOfFunctions()->FindObject("palette");
+                if (palette) palette->SetY2NDC(0.65);
+                gPad->Modified();
+                gPad->Update();
+            }
+        }
+
+        MainCanvas->Print(Histogram_OutPDF_fileName_char, "pdf");
+        MainCanvas->Clear();
+    }
+
+    sprintf(Histogram_OutPDF_fileName_char, "%s]", Histogram_OutPDF_fileName_str.c_str());
+    MainCanvas->Print(Histogram_OutPDF_fileName_char, "pdf");
+}
+
 };  // namespace histogram_functions
 
 #endif  // HISTOGRAM_FUNCTIONS_H
