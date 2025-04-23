20a751cc3bf19874af4f6fb8db9651df22515d5d
alons126 <alonsportes@mail.tau.ac.il>
2025-04-23 17:06:24 +0300

    V14_R1_P_E_miss_in_QE_theta_q_pCD

--- framework/classes/Settings/CustomPlotsDirectory.h
+++ framework/classes/Settings/CustomPlotsDirectory.h
@@ -27,7 +27,7 @@ class CustomPlotsDirectory {
 
     std::string Added_PreStatuses, Added_Statuses;
 
-    std::string Global_Run_Status = "V14_R1_theta_q_pCD";
+    std::string Global_Run_Status = "V14_R1_P_E_miss_in_QE_theta_q_pCD";
 
    public:
     // Default constructor ---------------------------------------------------------------------------------------------------------------------------------------------
