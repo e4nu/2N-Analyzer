c09bbe91818072cefa630f2d2253990188a7c999
alons126 <alonsportes@mail.tau.ac.il>
2025-04-23 15:02:26 +0300

    C12 G18 2GeV reac mon test 5

--- framework/classes/Settings/CustomPlotsDirectory.h
+++ framework/classes/Settings/CustomPlotsDirectory.h
@@ -27,7 +27,7 @@ class CustomPlotsDirectory {
 
     std::string Added_PreStatuses, Added_Statuses;
 
-    std::string Global_Run_Status = "ChainRunV13_R1";
+    std::string Global_Run_Status = "ChainRunV13_R1_xB_test";
 
    public:
     // Default constructor ---------------------------------------------------------------------------------------------------------------------------------------------
