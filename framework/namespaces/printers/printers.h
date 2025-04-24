c743d67e9dff9e473bca1625cd0b3c1419dfb525
alons126 <alonsportes@mail.tau.ac.il>
2025-04-24 10:39:44 +0300

    reruns noNC cuts

--- framework/namespaces/printers/printers.h
+++ framework/namespaces/printers/printers.h
@@ -105,6 +105,8 @@ void SavePlotsToZipFile(const bool& Save_Plots_folder_to_zip_files, const bool& 
             std::cout << "zip_filename: " << zip_filename << "\n";
             std::cout << "zip_command: " << zip_command << "\n";
         }
+
+        std::cout << "\n\n";
     } else {
         std::cout << "\n\n\033[33mSaving plots folder to zip file is disabled...\n\033[0m";
         return;
