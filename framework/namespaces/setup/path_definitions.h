d8dffa2d0d0a1997715c4588df7993152a6de676
alons126 <alonsportes@mail.tau.ac.il>
2025-04-25 19:28:34 +0300

    V15_KC_ExtEmiss_P_E_miss_in_MECandSRC_theta_q_pCD

--- framework/namespaces/setup/path_definitions.h
+++ framework/namespaces/setup/path_definitions.h
@@ -19,7 +19,7 @@ const std::string log_file_name = "Run_log.txt";
 const std::string plots_log_save_Directory = PathDefinitions.plots_path + "/" + log_file_name;
 const std::string skipped_files_list_save_Directory = PathDefinitions.plots_path + "/skipped_files_list.txt";
 
-const std::string custom_plots_path_prefix = "V15_KC_ExtEmiss_P_E_miss_in_MECandSRC";
+const std::string custom_plots_path_prefix = "V15_KC_ExtEmiss_P_E_miss_in_MECandSRC_theta_q_pCD";
 }  // namespace path_definitions
 
 #endif  // PATH_DEFINITIONS_H
