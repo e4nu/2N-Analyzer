f7e761a506a7671ec680c8a276e485c179b54e01
alons126 <alonsportes@mail.tau.ac.il>
2025-04-25 19:09:01 +0300

    V15_KC

--- framework/namespaces/setup/path_definitions.h
+++ framework/namespaces/setup/path_definitions.h
@@ -19,7 +19,7 @@ const std::string log_file_name = "Run_log.txt";
 const std::string plots_log_save_Directory = PathDefinitions.plots_path + "/" + log_file_name;
 const std::string skipped_files_list_save_Directory = PathDefinitions.plots_path + "/skipped_files_list.txt";
 
-const std::string custom_plots_path_prefix = "V15";
+const std::string custom_plots_path_prefix = "V15_KC";
 }  // namespace path_definitions
 
 #endif  // PATH_DEFINITIONS_H
