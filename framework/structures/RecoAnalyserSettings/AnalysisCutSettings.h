6d75db315a976cd46d9f6c8eca4f001d7512d8bd
alons126 <alonsportes@mail.tau.ac.il>
2025-04-25 19:26:09 +0300

    V15_KC_ExtEmiss_P_E_miss_in_QE

--- framework/structures/RecoAnalyserSettings/AnalysisCutSettings.h
+++ framework/structures/RecoAnalyserSettings/AnalysisCutSettings.h
@@ -106,7 +106,7 @@ struct AnalysisCutSettings {
           apply_nucleon_physical_cuts(true),
           apply_nBeta_fit_cuts(true),
           apply_fiducial_cuts(false),
-          apply_kinematical_cuts(false),
+          apply_kinematical_cuts(true),
           apply_kinematical_weights(false),
           apply_nucleon_SmearAndCorr(false),
 
@@ -114,10 +114,10 @@ struct AnalysisCutSettings {
           apply_reaction_monitoring_cuts(true),
           apply_P_miss_in_QE_range_cuts(true),
           apply_E_miss_in_QE_range_cuts(true),
-          apply_P_miss_in_MECandSRC_range_cuts(true),
-          apply_E_miss_in_MECandSRC_range_cuts(true),
+          apply_P_miss_in_MECandSRC_range_cuts(false),
+          apply_E_miss_in_MECandSRC_range_cuts(false),
           apply_xB_in_QE_range_cuts(false),
-          apply_theta_q_pCD_in_lower_FSI_range_cut(true),
+          apply_theta_q_pCD_in_lower_FSI_range_cut(false),
 
           custom_cuts_naming(true) {}
 
