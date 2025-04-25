dbf5f52d667a7a2a086306f293b3926063f3d16b
alons126 <alonsportes@mail.tau.ac.il>
2025-04-25 19:01:35 +0300

    V15_P_E_miss_in_MECandSRC

--- framework/structures/RecoAnalyserSettings/AnalysisCutSettings.h
+++ framework/structures/RecoAnalyserSettings/AnalysisCutSettings.h
@@ -112,10 +112,10 @@ struct AnalysisCutSettings {
 
           // Reaction monitoring cuts:
           apply_reaction_monitoring_cuts(true),
-          apply_P_miss_in_QE_range_cuts(true),
-          apply_E_miss_in_QE_range_cuts(true),
-          apply_P_miss_in_MECandSRC_range_cuts(false),
-          apply_E_miss_in_MECandSRC_range_cuts(false),
+          apply_P_miss_in_QE_range_cuts(false),
+          apply_E_miss_in_QE_range_cuts(false),
+          apply_P_miss_in_MECandSRC_range_cuts(true),
+          apply_E_miss_in_MECandSRC_range_cuts(true),
           apply_xB_in_QE_range_cuts(false),
           apply_theta_q_pCD_in_lower_FSI_range_cut(false),
 
