9ab494824d74abcff3ee1c10bfdd818df0aab1a7
alons126 <alonsportes@mail.tau.ac.il>
2025-04-26 15:56:58 +0300

    C12_2GeV_G18 V15_1_theta_q_pCD

--- framework/namespaces/setup/analysis_samples/analysis_samples.h
+++ framework/namespaces/setup/analysis_samples/analysis_samples.h
@@ -101,8 +101,8 @@ void ConfigSampleChain() {
     // Simulation samples -----------------------------------------------------------------------------------------------------------------------------------------------
 
     // // /* C12 G18 2GeV */
-    Config_C12_G18_2GeV_samples(AnalyzeFilePath_v, AnalyzeFileSample_v, AnalyzeFileDir_v, AnalyzeFile_v, AnalyzeFilePath, AnalyzeFileSample, AnalyzeFileDir, AnalyzeFile, false);  // no FC
-    // Config_C12_G18_2GeV_samples(AnalyzeFilePath_v, AnalyzeFileSample_v, AnalyzeFileDir_v, AnalyzeFile_v, AnalyzeFilePath, AnalyzeFileSample, AnalyzeFileDir, AnalyzeFile, true);   // with FC
+    // Config_C12_G18_2GeV_samples(AnalyzeFilePath_v, AnalyzeFileSample_v, AnalyzeFileDir_v, AnalyzeFile_v, AnalyzeFilePath, AnalyzeFileSample, AnalyzeFileDir, AnalyzeFile, false);  // no FC
+    Config_C12_G18_2GeV_samples(AnalyzeFilePath_v, AnalyzeFileSample_v, AnalyzeFileDir_v, AnalyzeFile_v, AnalyzeFilePath, AnalyzeFileSample, AnalyzeFileDir, AnalyzeFile, true);   // with FC
 
     // // /* C12 G18 4GeV */
     // Config_C12_G18_4GeV_samples(AnalyzeFilePath_v, AnalyzeFileSample_v, AnalyzeFileDir_v, AnalyzeFile_v, AnalyzeFilePath, AnalyzeFileSample, AnalyzeFileDir, AnalyzeFile, false);  // no FC
