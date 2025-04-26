025090a265f421dad2a645c1523ce14286f480bd
alons126 <alonsportes@mail.tau.ac.il>
2025-04-26 16:38:20 +0300

    V15_1_P_E_miss_in_QEandMECandSRC

--- framework/structures/RecoAnalyserSettings/AnalysisCutSettings.h
+++ framework/structures/RecoAnalyserSettings/AnalysisCutSettings.h
@@ -157,23 +157,23 @@ struct AnalysisCutSettings {
         //   apply_xB_in_QE_range_cuts(false),
         //   apply_theta_q_pCD_in_lower_FSI_range_cut(true),
 
-        //   /* P_E_miss_in_QEandMECandSRC */
-        //   apply_reaction_monitoring_cuts(true),
-        //   apply_P_miss_in_QE_range_cuts(true),
-        //   apply_E_miss_in_QE_range_cuts(true),
-        //   apply_P_miss_in_MECandSRC_range_cuts(true),
-        //   apply_E_miss_in_MECandSRC_range_cuts(true),
-        //   apply_xB_in_QE_range_cuts(false),
-        //   apply_theta_q_pCD_in_lower_FSI_range_cut(false),
-
-          /* P_E_miss_in_QEandMECandSRC_theta_q_pCD */
+          /* P_E_miss_in_QEandMECandSRC */
           apply_reaction_monitoring_cuts(true),
           apply_P_miss_in_QE_range_cuts(true),
           apply_E_miss_in_QE_range_cuts(true),
           apply_P_miss_in_MECandSRC_range_cuts(true),
           apply_E_miss_in_MECandSRC_range_cuts(true),
           apply_xB_in_QE_range_cuts(false),
-          apply_theta_q_pCD_in_lower_FSI_range_cut(true),
+          apply_theta_q_pCD_in_lower_FSI_range_cut(false),
+
+        //   /* P_E_miss_in_QEandMECandSRC_theta_q_pCD */
+        //   apply_reaction_monitoring_cuts(true),
+        //   apply_P_miss_in_QE_range_cuts(true),
+        //   apply_E_miss_in_QE_range_cuts(true),
+        //   apply_P_miss_in_MECandSRC_range_cuts(true),
+        //   apply_E_miss_in_MECandSRC_range_cuts(true),
+        //   apply_xB_in_QE_range_cuts(false),
+        //   apply_theta_q_pCD_in_lower_FSI_range_cut(true),
 
         //   /* theta_q_pCD */
         //   apply_reaction_monitoring_cuts(true),
