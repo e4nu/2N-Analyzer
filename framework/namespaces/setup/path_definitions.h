581f7b862d93de944e68c5c49b7d7ae3ee95bef4
alons126 <alonsportes@mail.tau.ac.il>
2025-04-26 16:25:26 +0300

    V15_1_ExtEmiss_P_E_miss_in_MECandSRC

--- framework/namespaces/setup/path_definitions.h
+++ framework/namespaces/setup/path_definitions.h
@@ -26,7 +26,7 @@ const std::string skipped_files_list_save_Directory = PathDefinitions.plots_path
 
 // const std::string custom_plots_path_prefix = "V15_1_ExtEmiss_P_E_miss_in_QE";
 // const std::string custom_plots_path_prefix = "V15_1_ExtEmiss_P_E_miss_in_QE_theta_q_pCD";
-// const std::string custom_plots_path_prefix = "V15_1_ExtEmiss_P_E_miss_in_MECandSRC";
+const std::string custom_plots_path_prefix = "V15_1_ExtEmiss_P_E_miss_in_MECandSRC";
 // const std::string custom_plots_path_prefix = "V15_1_ExtEmiss_P_E_miss_in_MECandSRC_theta_q_pCD";
 // const std::string custom_plots_path_prefix = "V15_1_ExtEmiss_P_E_miss_in_QEandMECandSRC";
 // const std::string custom_plots_path_prefix = "V15_1_ExtEmiss_P_E_miss_in_QEandMECandSRC_theta_q_pCD";
@@ -37,7 +37,7 @@ const std::string skipped_files_list_save_Directory = PathDefinitions.plots_path
 // const std::string custom_plots_path_prefix = "V15_1_P_E_miss_in_MECandSRC";
 // const std::string custom_plots_path_prefix = "V15_1_P_E_miss_in_MECandSRC_theta_q_pCD";
 // const std::string custom_plots_path_prefix = "V15_1_P_E_miss_in_QEandMECandSRC";
-const std::string custom_plots_path_prefix = "V15_1_P_E_miss_in_QEandMECandSRC_theta_q_pCD";
+// const std::string custom_plots_path_prefix = "V15_1_P_E_miss_in_QEandMECandSRC_theta_q_pCD";
 // const std::string custom_plots_path_prefix = "V15_1_theta_q_pCD";
 
 }  // namespace path_definitions
