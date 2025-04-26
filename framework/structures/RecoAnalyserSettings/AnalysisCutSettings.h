c800900ef76cac7c017bf354cef96f241b14dd57
alons126 <alonsportes@mail.tau.ac.il>
2025-04-26 21:01:28 +0300

    V16_P_E_miss_in_QE

--- framework/structures/RecoAnalyserSettings/AnalysisCutSettings.h
+++ framework/structures/RecoAnalyserSettings/AnalysisCutSettings.h
@@ -112,23 +112,23 @@ struct AnalysisCutSettings {
 
           // Reaction monitoring cuts:
 
-          /* Regular */
-          apply_reaction_monitoring_cuts(false),
-          apply_P_miss_in_QE_range_cuts(false),
-          apply_E_miss_in_QE_range_cuts(false),
-          apply_P_miss_in_MECandSRC_range_cuts(false),
-          apply_E_miss_in_MECandSRC_range_cuts(false),
-          apply_xB_in_QE_range_cuts(false),
-          apply_theta_q_pCD_in_lower_FSI_range_cut(false),
-
-        //   /* P_E_miss_in_QE */
-        //   apply_reaction_monitoring_cuts(true),
-        //   apply_P_miss_in_QE_range_cuts(true),
-        //   apply_E_miss_in_QE_range_cuts(true),
+        //   /* Regular */
+        //   apply_reaction_monitoring_cuts(false),
+        //   apply_P_miss_in_QE_range_cuts(false),
+        //   apply_E_miss_in_QE_range_cuts(false),
         //   apply_P_miss_in_MECandSRC_range_cuts(false),
         //   apply_E_miss_in_MECandSRC_range_cuts(false),
         //   apply_xB_in_QE_range_cuts(false),
         //   apply_theta_q_pCD_in_lower_FSI_range_cut(false),
+
+          /* P_E_miss_in_QE */
+          apply_reaction_monitoring_cuts(true),
+          apply_P_miss_in_QE_range_cuts(true),
+          apply_E_miss_in_QE_range_cuts(true),
+          apply_P_miss_in_MECandSRC_range_cuts(false),
+          apply_E_miss_in_MECandSRC_range_cuts(false),
+          apply_xB_in_QE_range_cuts(false),
+          apply_theta_q_pCD_in_lower_FSI_range_cut(false),
 
         //   /* P_E_miss_in_QE_theta_q_pCD */
         //   apply_reaction_monitoring_cuts(true),
