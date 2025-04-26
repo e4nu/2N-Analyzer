afa741bee2ef8c19f183683917403b9df08d92ec
alons126 <alonsportes@mail.tau.ac.il>
2025-04-26 21:02:42 +0300

    V16_P_E_miss_in_MECandSRC

--- framework/structures/RecoAnalyserSettings/AnalysisCutSettings.h
+++ framework/structures/RecoAnalyserSettings/AnalysisCutSettings.h
@@ -130,23 +130,23 @@ struct AnalysisCutSettings {
         //   apply_xB_in_QE_range_cuts(false),
         //   apply_theta_q_pCD_in_lower_FSI_range_cut(false),
 
-          /* P_E_miss_in_QE_theta_q_pCD */
-          apply_reaction_monitoring_cuts(true),
-          apply_P_miss_in_QE_range_cuts(true),
-          apply_E_miss_in_QE_range_cuts(true),
-          apply_P_miss_in_MECandSRC_range_cuts(false),
-          apply_E_miss_in_MECandSRC_range_cuts(false),
-          apply_xB_in_QE_range_cuts(false),
-          apply_theta_q_pCD_in_lower_FSI_range_cut(true),
-
-        //   /* P_E_miss_in_MECandSRC */
+        //   /* P_E_miss_in_QE_theta_q_pCD */
         //   apply_reaction_monitoring_cuts(true),
-        //   apply_P_miss_in_QE_range_cuts(false),
-        //   apply_E_miss_in_QE_range_cuts(false),
-        //   apply_P_miss_in_MECandSRC_range_cuts(true),
-        //   apply_E_miss_in_MECandSRC_range_cuts(true),
+        //   apply_P_miss_in_QE_range_cuts(true),
+        //   apply_E_miss_in_QE_range_cuts(true),
+        //   apply_P_miss_in_MECandSRC_range_cuts(false),
+        //   apply_E_miss_in_MECandSRC_range_cuts(false),
         //   apply_xB_in_QE_range_cuts(false),
-        //   apply_theta_q_pCD_in_lower_FSI_range_cut(false),
+        //   apply_theta_q_pCD_in_lower_FSI_range_cut(true),
+
+          /* P_E_miss_in_MECandSRC */
+          apply_reaction_monitoring_cuts(true),
+          apply_P_miss_in_QE_range_cuts(false),
+          apply_E_miss_in_QE_range_cuts(false),
+          apply_P_miss_in_MECandSRC_range_cuts(true),
+          apply_E_miss_in_MECandSRC_range_cuts(true),
+          apply_xB_in_QE_range_cuts(false),
+          apply_theta_q_pCD_in_lower_FSI_range_cut(false),
 
         //   /* P_E_miss_in_MECandSRC_theta_q_pCD */
         //   apply_reaction_monitoring_cuts(true),
