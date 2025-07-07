0af7d247c76de8761a471f73bb742df163a65618
alons126 <alonsportes@mail.tau.ac.il>
2025-07-07 09:47:33 +0300

    C12_SuSa_2GeV for julia

--- applications/RecoAnalyzer.cpp
+++ applications/RecoAnalyzer.cpp
@@ -219,8 +219,9 @@ RecoAnalyzer::RecoAnalyzer(const std::string &AnalyzeFilePath, const std::string
 
     bool TestRun = false;       // set as false for a full run
     bool ApplyLimiter = false;  // set as false for a full run
+    int Limiter = 10000000;
     // int Limiter = 5000000;
-    int Limiter = 1000000;
+    // int Limiter = 1000000;
     // int Limiter = 500000;
 
     // Set enabled plots
